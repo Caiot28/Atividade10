c28212c9756d301717be1ea417e0eac15eb1a3aa
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2023-10-23 15:10:16 -0300

    Add files via upload

--- Exercicio28.c
+++ Exercicio28.c
@@ -2,47 +2,33 @@
 
 main(){
 
-    int vetor1[10], tamanho1 = 0, tamanho2 = 0, v2[tamanho1], v3[tamanho2];
+    int v[10], tamanho1 = 0, tamanho2 = 0, v1[10], v2[10];
 
-       for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 10; i++){
         printf("Digite um numero: ");
-        scanf("%d", &vetor1[i]);
+        scanf("%d", &v[i]);
 
-    
-        if(vetor1[i] % 2 == 0){
-            tamanho1++;
-            v2[i] = vetor1[i];
-        } else {
+            if(v[i] % 2 == 0){
+            v2[tamanho2] = v[i];
             tamanho2++;
-            v3[i] = vetor1[i];
+            } else {
+            v1[tamanho1] = v[i];
+            tamanho1++;
+            }
+
         }
 
-    }
-    // int v2[tamanho1], v3[tamanho2];
+        for (int i = 0; i < 10; i++) { 
+        printf("\nV1: %d", v[i]);
+        }
 
-    // for(int i = 0; i < 10; i++){
-    //     for(int j = 0; j < tamanho1; j++){
-    //         if(vetor1[i] % 2 == 0){
-    //             v2[j] = vetor1[i];
-    //         } else {
-    //             v3[j] = vetor1[i];
-    //         }
-    //     }
-    // }
+        for (int i = 0; i < tamanho1; i++) { 
+        printf("\nV2: %d", v1[i]);
+        }
 
-    for (int i = 0; i < 10; i++) { 
-    printf("\nV1: %d", vetor1[i]);
-    }
-
-    for (int i = 0; i < tamanho1; i++) { 
-    printf("\nV2: %d", v2[i]);
-    }
-
-    for (int i = 0; i < tamanho2; i++) { 
-    printf("\nV3: %d", v3[i]);
-    }
-
-
-
+        for (int i = 0; i < tamanho2; i++) { 
+        printf("\nV3: %d", v2[i]);
+        }
 
 }
+
